e2e6ccd19f8ce5fa5c0916ae42225d9ed7d0cadd
iserifith <seriputera2@gmail.com>
2018-06-12 15:49:17 +0800

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import logger from "morgan";
+import bodyParser from "body-parser";
+import routes from "./routes";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+routes(app);
+
+const PORT = process.env.PORT || 4450;
+
+app.listen(PORT, () => {
+	console.log(`App is running on port ${PORT}`);
+});
